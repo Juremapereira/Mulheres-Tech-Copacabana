77f80ab1b1b8006a1a187b4a3a7dd8fc5cdb8779
Juremapereira <Juremasilvapereira@outlook.com>
2024-09-20 21:26:37 -0300

    arquivo do curso de análises de dados-(aula 01 02)

---
+++ aula01.py
@@ -0,0 +1,38 @@
+"""print("digite um numero:")
+valor = int(input()    #criando uma variável que vai solicitar o usuario a digitar o numero
+dobro =valor * 2  #solicitando um resultado  
+print("o valor do dobro é" , dobro) # requerimento do resultado """
+
+'''print("digite a nota do primeiro bimestre:")
+b1 = float(input())
+print("digite a nota do segundo bimestre:")
+b2 = float(input())
+print("digite a nota do terceiro bimestre:")
+b3 = float(input())
+print("digita a nota do quarto bimestre:")
+b4 = float(input())
+media =(b1 + b2 + b3 + b4) / 4
+print("a media do aluno é:",media)'''
+
+
+#código para o usuario inserir dois numeros e retornar todas as operações básicas e módulos
+
+print("mulheres tech copacabana")
+print("digite o primeiro número")
+N1 = int(input())
+10print("digite o segundo número")
+N2 = int(input())
+
+soma = N1 + N2
+subtracao = N1 - N2
+multiplicacao = N1 * N2
+divisao = N1 / N2
+modulo = N1 % N2
+
+print("o resultado da soma é: " ,soma)
+print("o resultado da subtração é: " ,subtracao)
+print("o resultado da multiplicacao é:" ,multiplicacao)
+print("o resultado da divisão é: " ,divisao)
+print("o resultado do módulo é: ", modulo )
+
+
